465c83cdc26740fddc09d2fec21f9a6bf8c22d89
Glen Willis <glenwillis@gmail.com>
2017-01-20 22:25:36 +0800

    More random assets.

---
+++ Assets/Scripts/FPSPlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPSPlayerControl : MonoBehaviour {
+
+    private RaycastHit hit;
+    private Camera fpsCam;
+    public Image Crosshair;
+    public Sprite ActiveCrosshair, NormalCrosshair;
+    private bool ActiveCrosshairSet = false;
+
+	void Start ()
+    {
+        fpsCam = GetComponentInChildren<Camera>();
+        Crosshair.sprite = NormalCrosshair;
+	}
+
+    void Update()
+    {
+        if (ActiveCrosshairSet)
+        {
+            Crosshair.sprite = NormalCrosshair;
+            ActiveCrosshairSet = false;
+        }
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, 100F))
+        {
+            if(hit.transform.gameObject.tag == "Clickable")
+            {
+                if (!ActiveCrosshairSet)
+                {
+                    Crosshair.sprite = ActiveCrosshair;
+                    ActiveCrosshairSet = true;
+                }
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    Debug.Log("Can click!");
+                }
+            }
+        }
+    }
+
+}
