34fff809fabef7407915882da0c0df9d2ced53a9
Glen Willis <glenwillis@gmail.com>
2017-01-21 10:07:58 +0800

    Adding the falling wall floor puzzle.

---
+++ Assets/Scripts/FallingFallTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingFallTrigger : MonoBehaviour {
+
+	public bool isWalkable;
+    FallingFloorControl ffc;
+    public float DelayForFloorFalling;
+
+    void Start()
+    {
+        ffc = GetComponentInParent<FallingFloorControl>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (ffc.WallPuzzleActive)
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                FallingWallAction(isWalkable);
+            }
+        }
+    }
+
+    private void FallingWallAction(bool isWalkable)
+    {
+        if (isWalkable)
+        {
+            Debug.Log("Safe");
+        }
+        else
+        {
+            StartCoroutine("DelayFloorFalling");
+        }
+
+    }
+
+    IEnumerator DelayFloorFalling()
+    {
+        yield return new WaitForSeconds(DelayForFloorFalling);
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/FallingFloorControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingFloorControl : MonoBehaviour {
+
+    public bool WallPuzzleActive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        WallPuzzleActive = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
