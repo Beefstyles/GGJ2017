40cd7eff30c6907d56caa04fc6185bb43a9c91a2
Glen Willis <glenwillis@gmail.com>
2017-01-21 14:23:26 +0800

    Added a simple rotate object script.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+    private float rotationSpeed = 50F;
+    private Quaternion targetRot;
+    
+	void Start ()
+    {
+        targetRot = transform.rotation;
+	}
+
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+	}
+
+}
