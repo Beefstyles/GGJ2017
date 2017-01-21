1fc11ff8495906bc0b9043ebeebf65c06ad28635
Glen Willis <glenwillis@gmail.com>
2017-01-21 12:15:50 +0800

    Added a move to floor light puzzle.

---
+++ Assets/Scripts/FallingFloorTiming.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingFloorTiming : MonoBehaviour {
+
+    //Color Blue, Green, Purple,Red, Teal, Yellow
+
+    public FloorTimingLightsPuzzle.LightColours lc;
+    private bool floorIsActive = true;
+
+    public void ReceiveLightColour(FloorTimingLightsPuzzle.LightColours lightColour)
+    {
+        if(lightColour == lc)
+        {
+            FloorDelayToTransparentAndBack(true);
+            floorIsActive = true;
+        }
+        else
+        {
+            if (floorIsActive)
+            {
+                FloorDelayToTransparentAndBack(false);
+            }
+        }
+    }
+
+
+    IEnumerator FloorDelayToTransparentAndBack(bool floorEnabled)
+    {
+        float alpha = transform.GetComponent<MeshRenderer>().material.color.a;
+        Color currentColour;
+        currentColour = transform.GetComponent<MeshRenderer>().material.color;
+        if (!floorEnabled)
+        {
+            for (float t = 0F; t < 0.5; t += Time.deltaTime)
+            {
+                currentColour.a = Mathf.Lerp(alpha, 0, t);
+                transform.GetComponent<MeshRenderer>().material.color = currentColour;
+                yield return null;
+            }
+            DisableFloor();
+        }
+
+        else
+        {
+            for (float t = 0F; t < .5; t += Time.deltaTime)
+            {
+                currentColour.a = Mathf.Lerp(0, alpha, t);
+                transform.GetComponent<MeshRenderer>().material.color = currentColour;
+                yield return null;
+            }
+            EnableFloor();
+        }
+    }
+
+    void DisableFloor()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void EnableFloor()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/FloorTimingLightsPuzzle.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FloorTimingLightsPuzzle : MonoBehaviour {
+
+    private Light mainSpotLight;
+    //Color Blue, Green, Purple,Red, Teal, Yellow
+
+    public enum LightColours
+    {
+        Blue, Green, Purple, Red, Teal, Yellow
+    };
+
+    public GameObject floors;
+    public FallingFloorTiming[] fallingFloorTimingObj;
+
+    private Color teal = new Color(0, 128, 128);
+
+    private LightColours chosenLightColour;
+
+    private float lightChangeTimer = 2F;
+
+    void Start ()
+    {
+        fallingFloorTimingObj = floors.GetComponentsInChildren<FallingFloorTiming>();
+        mainSpotLight = GetComponentInChildren<Light>();
+        mainSpotLight.color = Color.blue;
+    }
+
+	void Update ()
+    {
+        if(lightChangeTimer >= 0)
+        {
+            lightChangeTimer -= Time.deltaTime;
+        }
+        else
+        {
+            PickRandomLightColour();
+            lightChangeTimer = 2;
+        }
+	}
+
+    void PickRandomLightColour()
+    {
+        chosenLightColour = (LightColours)Random.Range(0, 5);
+        ChangeLightColour(chosenLightColour);
+        SendLightColour(chosenLightColour);
+    }
+
+    private void ChangeLightColour(LightColours chosenLightColour)
+    {
+        switch (chosenLightColour)
+        {
+            case (LightColours.Blue):
+                mainSpotLight.color = Color.blue;
+                break;
+            case (LightColours.Green):
+                mainSpotLight.color = Color.green;
+                break;
+            case (LightColours.Purple):
+                mainSpotLight.color = Color.magenta;
+                break;
+            case (LightColours.Red):
+                mainSpotLight.color = Color.red;
+                break;
+            case (LightColours.Teal):
+                mainSpotLight.color = teal;
+                break;
+            case (LightColours.Yellow):
+                mainSpotLight.color = Color.yellow;
+                break;
+        }
+    }
+
+    private void SendLightColour(LightColours chosenLightColour)
+    {
+        foreach(FallingFloorTiming floor in fallingFloorTimingObj)
+        {
+            floor.ReceiveLightColour(chosenLightColour);
+        }
+    }
+}
